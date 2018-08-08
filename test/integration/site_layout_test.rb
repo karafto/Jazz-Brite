b4c313711466598bbdc2270313d84febbb76a58a
Kevin Raftogianis <kevinarafto@gmail.com>
2018-08-07 18:43:38 -0700

    Style text

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -7,7 +7,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, text: "Jazz-Brite"
     assert_select "a[href=?]", events_path, text: "Attend Events"
-    assert_select "a[href=?]", events_path, text: "All Events"
+    assert_select "a[href=?]", events_path, text: "ALL EVENTS"
     assert_select "a[href=?]", new_event_path, text: "Create Events"
     assert_select "a[href=?]", new_event_path, text: "Create Event"
     assert_select "a[href=?]", new_user_registration_path, text: "Sign Up"
