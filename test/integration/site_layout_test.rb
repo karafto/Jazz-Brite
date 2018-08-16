97ebc820f338d90217b93aacf3c6f6ccfda0a232
Kevin Raftogianis <kevinarafto@gmail.com>
2018-08-15 19:06:50 -0700

    Re-style home page

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,6 +8,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", root_path, text: "Jazz-Brite"
     assert_select "a[href=?]", events_path, text: "Attend Events"
     assert_select "a[href=?]", events_path, text: "ALL EVENTS"
+    assert_select "a[href=?]", events_path, text: "Start exploring"
     assert_select "a[href=?]", new_event_path, text: "Create Events"
     assert_select "a[href=?]", new_event_path, text: "Create Event"
     assert_select "a[href=?]", new_user_registration_path, text: "Sign Up"
