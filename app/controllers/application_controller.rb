cc6b6b2e6a3957bcb719377aa11db9d7a9edcd15
Kevin Raftogianis <kevinarafto@gmail.com>
2018-09-22 17:35:41 -0700

    Implement redirect to previous page after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :store_user_location!, if: :storable_location?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(user)
-    events_path
+    stored_location_for(user) || super
   end
 
   def after_sign_out_path_for(user)
@@ -15,6 +16,14 @@ class ApplicationController < ActionController::Base
       added_attrs = [:name, :email, :password, :password_confirmation, :remember_me]
       devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
       devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    end
+
+    def storable_location?
+      request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
+    end
+
+    def store_user_location!
+      store_location_for(:user, request.fullpath)
     end
 
     def authorized?(resource)
