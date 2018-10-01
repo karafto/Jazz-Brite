c2c11b96ac084fda65dabf36c169d432e8ef51a4
Kevin Raftogianis <kevinarafto@gmail.com>
2018-09-30 20:47:12 -0700

    Change site layout test to reflect navbar change

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -7,7 +7,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, text: "Jazz-Brite"
     assert_select "a[href=?]", events_path, text: "Attend Events"
-    assert_select "a[href=?]", events_path, text: "ALL EVENTS"
+    assert_select "a[href=?]", events_path, text: "Browse Events"
     assert_select "a[href=?]", events_path, text: "Start exploring"
     assert_select "a[href=?]", new_event_path, text: "Create Events"
     assert_select "a[href=?]", new_event_path, text: "Create Event"
