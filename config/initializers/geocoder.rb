d9d35b44a7ba062115dd6cd03e1f92b65c1d9a56
Kevin Raftogianis <kevinarafto@gmail.com>
2019-01-29 19:53:50 -0800

    Add Ipinfo API key

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -2,12 +2,13 @@ Geocoder.configure(
   # Geocoding options
     timeout: 10,                # geocoding service timeout (secs)
     lookup: :google,            # name of geocoding service (symbol)
-  # ip_lookup: :ipinfo_io,      # name of IP address geocoding service (symbol)
+    ip_lookup: :ipinfo_io,      # name of IP address geocoding service (symbol)
   # language: :en,              # ISO-639 language code
     use_https: true,            # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
     google: {api_key: ENV['GOOGLE_API_KEY']},        # API key for geocoding service
+    ipinfo_io: {api_key: ENV['IPINFO_API_KEY']},
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
